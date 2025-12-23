1fbc5a5a61fe04d9b4d0d3e9e0bc873a635b2271
Adam Pangelinan <adamtpangelinan@gmail.com>
2025-12-23 16:16:47 +0800

    Fix vercel deploy

--- app/api/exa_website_scrape/route.ts
+++ app/api/exa_website_scrape/route.ts
@@ -4,28 +4,27 @@ import Exa from "exa-js";
 
 export const maxDuration = 60;
 
-const exa = new Exa(process.env.EXA_API_KEY as string);
-
 export async function POST(req: NextRequest) {
   try {
+    const exa = new Exa(process.env.EXA_API_KEY as string);
     const { websiteurl } = await req.json();
     if (!websiteurl) {
       return NextResponse.json({ error: 'Website URL is required' }, { status: 400 });
     }
 
-      const result = await exa.searchAndContents(
-        websiteurl,
-        {
-          type: "auto",
-          text: true,
-          numResults: 1,
-          // @ts-ignore
-          livecrawl: "preferred",
-          subpages: 5,
-          subpageTarget: ["about", "pricing", "faq", "blog"],
-          includeDomains: [websiteurl]
-        }
-      )
+    const result = await exa.searchAndContents(
+      websiteurl,
+      {
+        type: "auto",
+        text: true,
+        numResults: 1,
+        // @ts-ignore
+        livecrawl: "preferred",
+        subpages: 5,
+        subpageTarget: ["about", "pricing", "faq", "blog"],
+        includeDomains: [websiteurl]
+      }
+    )
 
     return NextResponse.json({ results: result.results });
   } catch (error) {
